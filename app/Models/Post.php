2ef7b2dc411fb96eb4e1285facca7abbe3a03ca4
= <=>
2023-07-01 05:07:37 +0800

    started working in the frontend

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use \Illuminate\Database\Eloquent\Relations\BelongsTo;
 use \Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -21,7 +23,9 @@ class Post extends Model
         'user_id'
     ];
 
-
+    protected $cast = [
+        'published_at' => 'datetime'
+    ];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class,);
@@ -30,5 +34,19 @@ class Post extends Model
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class);
+    }
+
+
+
+    public function getFormattedDate() 
+    {
+        $date = Carbon::parse($this->published_at);
+        return $date->format('F j, Y');
+    }
+    
+
+    public function shortBody(): string 
+    {
+        return Str::words(strip_tags($this->body), 55);
     }
 }
---
+++ resources/views/components/post-item.blade.php
@@ -0,0 +1,19 @@
+<article class="flex flex-col shadow my-4" style="border-top-left-radius: 10px; border-top-right-radius: 10px">
+    <!-- Article Image -->
+    <a href="#" class="hover:opacity-75">
+        <img src="{{ url('storage/' . $post->thumbnail) }}" alt="Featured Image" style="object-fit: cover; width: 100%; height: 500px; border-top-left-radius: 10px; border-top-right-radius: 10px;">
+    </a>
+    <div class="bg-white flex flex-col justify-start p-6">
+        @foreach ($post->categories as $category)
+            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">
+                    {{ $category->title }}  
+            </a>
+        @endforeach
+        <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $post->title }}</a>
+        <p href="#" class="text-sm pb-3">
+            By <a href="#" class="font-semibold hover:text-gray-800">{{ $post->user->name }}</a>, Published on {{ $post->getFormattedDate() }}
+        </p>
+        <a href="#" class="pb-6">{{ $post->shortBody() }}</a>
+        <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
+    </div>
+</article>
